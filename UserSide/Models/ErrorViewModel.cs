bb30fa0bf6203d54de54cf49662c38725fc92c63
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2018-11-30 22:49:40 +0800

    Added User and Admin Projects

---
+++ UserSide/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPJ_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
