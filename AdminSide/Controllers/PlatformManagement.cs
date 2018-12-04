ae74d9a1f72c049a2feb313efad8767fd0269554
Wheatley <30753183+hanlim83@users.noreply.github.com>
2018-12-04 15:49:41 +0800

    Added Platform Management Area

---
+++ AdminSide/Controllers/PlatformManagement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPJ_MVC.Models;
+
+namespace ASPJ_MVC.Controllers
+{
+    public class PlatformManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
