a3814a965317e00ddc8fc1683aa547b348c0326a
Wheatley <30753183+hanlim83@users.noreply.github.com>
2018-12-12 00:38:55 +0800

    Resolved caching issue (Changes done to Layout)
    Issue with caching may result in unexpected results

--- UserSide/wwwroot/js/site.js
+++ UserSide/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    var url = window.location;
+    $('.navbar .nav').find('.active').removeClass('active');
+    $('.navbar .nav li a').each(function () {
+        if (this.href == url) {
+            $(this).parent().addClass('active');
+        }
+    });
+});
