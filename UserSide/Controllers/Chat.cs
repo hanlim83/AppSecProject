b68ad4e717eb2fce4c547653437ff07a5d42f5c6
Yakzhisheng <44362452+Yakzhisheng@users.noreply.github.com>
2018-12-18 17:22:14 +0800

    Base of Chat

--- UserSide/Controllers/Chat.cs
+++ UserSide/Controllers/Chat.cs
@@ -9,8 +9,7 @@ namespace UserSide.Controllers
     public class Chat : Controller
     {
         public IActionResult Index()
-        {
-            ViewData["Message"] = "Lets talk";
+        { 
             return View();
         }
     }
---
+++ UserSide/Models/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSide.Models
+{
+    public class Chat
+    {
+        private string Name { get; set; }
+        private int Num { get; set; }
+        private string status { get; set; }
+
+    }
+}
