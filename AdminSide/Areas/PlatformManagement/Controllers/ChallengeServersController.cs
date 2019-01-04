522df770e0c85f5bed5eb9f9529a349ce50026a1
Wheatley <30753183+hanlim83@users.noreply.github.com>
2019-01-05 01:28:47 +0800

    Initial Server Creation module

--- AdminSide/Areas/PlatformManagement/Controllers/ChallengeServersController.cs
+++ AdminSide/Areas/PlatformManagement/Controllers/ChallengeServersController.cs
@@ -10,6 +10,8 @@ using AdminSide.Areas.PlatformManagement.Models;
 using System.Diagnostics;
 using ASPJ_MVC.Models;
 using Microsoft.EntityFrameworkCore;
+using Subnet = AdminSide.Areas.PlatformManagement.Models.Subnet;
+using System.Net;
 
 namespace AdminSide.Areas.PlatformManagement.Controllers
 {
@@ -19,6 +21,8 @@ namespace AdminSide.Areas.PlatformManagement.Controllers
         private readonly PlatformResourcesContext _context;
 
         IAmazonEC2 EC2Client { get; set; }
+
+        private ChallengeServersCreationFormModel creationReference;
 
         public ChallengeServersController(PlatformResourcesContext context, IAmazonEC2 ec2Client)
         {
@@ -36,17 +40,164 @@ namespace AdminSide.Areas.PlatformManagement.Controllers
             return View(await _context.Templates.ToListAsync());
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> SelectTemplate(String selectedTemplate)
+        public IActionResult SpecifySettings()
         {
-            TempData["selectedTemplate"] = selectedTemplate;
-            return RedirectToAction("SpecifySettings");
+            return RedirectToAction("SelectTemplate");
         }
 
-        public async Task<IActionResult> SpecifySettings()
+        public IActionResult VerifySettings()
         {
-            return View(await _context.Subnets.ToListAsync());
+            if (creationReference == null)
+                return RedirectToAction("SelectTemplate");
+            else
+                return View(creationReference);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SpecifySettings(String selectedTemplate)
+        {
+            if (selectedTemplate != null)
+            {
+                TempData["selectedTemplate"] = selectedTemplate;
+                return View(await _context.Subnets.ToListAsync());
+            }
+            else
+                return RedirectToAction("SelectTemplate");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> VerifySettings(String ServerName, String ServerWorkload, String SelectedTemplate, String ServerStorage, String ServerTenancy, String ServerSubnet)
+        {
+            creationReference = new ChallengeServersCreationFormModel
+            {
+                ServerName = ServerName,
+                ServerWorkload = ServerWorkload,
+                selectedTemplate = await _context.Templates.FindAsync(Int32.Parse(SelectedTemplate)),
+                ServerStorage = Int32.Parse(ServerStorage),
+                ServerTenancy = ServerTenancy,
+                ServerSubnet = await _context.Subnets.FindAsync(Int32.Parse(ServerSubnet))
+            };
+            TempData["TemplateID"] = creationReference.selectedTemplate.ID;
+            TempData["SubnetID"] = creationReference.ServerSubnet.ID;
+            return View(creationReference);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Submission([Bind("TemplateName,OperatingSystem,ServerName,ServerWorkload,ServerStorage,ServerTenancy,SubnetCIDR,TemplateID,SubnetID")] ChallengeServersCreationFormModel Model2)
+        {
+            Template selectedT = await _context.Templates.FindAsync(Int32.Parse(Model2.TemplateID));
+            Subnet selectedS = await _context.Subnets.FindAsync(Int32.Parse(Model2.SubnetID));
+            RunInstancesRequest request = new RunInstancesRequest
+            {
+                BlockDeviceMappings = new List<BlockDeviceMapping> {
+                        new BlockDeviceMapping {
+                            DeviceName = "/dev/sdh",
+                            Ebs = new EbsBlockDevice { VolumeSize = Model2.ServerStorage }
+                        }
+                    },
+                ImageId = selectedT.AWSAMIReference,
+                KeyName = "ASPJ Instances Master Key Pair",
+                MaxCount = 1,
+                MinCount = 1,
+                SubnetId = selectedS.AWSVPCSubnetReference
+            };
+            if (Model2.ServerWorkload.Equals("Low"))
+            {
+                request.InstanceType = "t2.micro";
+                if (Model2.ServerTenancy.Equals("Dedicated Instance"))
+                {
+                    request.Placement = new Placement
+                    {
+                        Tenancy = Tenancy.Dedicated
+                    };
+                }
+                else if (Model2.ServerTenancy.Equals("Dedicated Hardware"))
+                {
+                    request.Placement = new Placement
+                    {
+                        Tenancy = Tenancy.Host
+                    };
+                }
+            }
+            else if (Model2.ServerWorkload.Equals("Medium"))
+            {
+                request.InstanceType = "t2.medium";
+                if (Model2.ServerTenancy.Equals("Dedicated Instance"))
+                {
+                    request.Placement = new Placement
+                    {
+                        Tenancy = Tenancy.Dedicated
+                    };
+                }
+                else if (Model2.ServerTenancy.Equals("Dedicated Hardware"))
+                {
+                    request.Placement = new Placement
+                    {
+                        Tenancy = Tenancy.Host
+                    };
+                }
+            }
+            else if (Model2.ServerWorkload.Equals("Large"))
+            {
+                request.InstanceType = "t2.xlarge";
+                if (Model2.ServerTenancy.Equals("Dedicated Instance"))
+                {
+                    request.Placement = new Placement
+                    {
+                        Tenancy = Tenancy.Dedicated
+                    };
+                }
+                else if (Model2.ServerTenancy.Equals("Dedicated Hardware"))
+                {
+                    request.Placement = new Placement
+                    {
+                        Tenancy = Tenancy.Host
+                    };
+                }
+            }
+            try
+            {
+                RunInstancesResponse response = await EC2Client.RunInstancesAsync(request);
+                if (response.HttpStatusCode.Equals(HttpStatusCode.OK))
+                {
+                    Server newlyCreated = new Server
+                    {
+                        Name = Model2.ServerName,
+                        OperatingSystem = selectedT.OperatingSystem,
+                        StorageAssigned = Model2.ServerStorage,
+                        DateCreated = DateTime.Now,
+                        AWSEC2Reference = response.Reservation.Instances[0].InstanceId,
+                        LinkedSubnet = selectedS,
+                        SubnetID = selectedS.ID
+                    };
+                    if (selectedS.Type == SubnetType.Internet)
+                    {
+                        newlyCreated.IPAddress = response.Reservation.Instances[0].PublicIpAddress;
+                        newlyCreated.DNSHostname = response.Reservation.Instances[0].PublicDnsName;
+                    }
+                    else
+                    {
+                        newlyCreated.IPAddress = response.Reservation.Instances[0].PrivateIpAddress;
+                        newlyCreated.DNSHostname = response.Reservation.Instances[0].PrivateDnsName;
+                    }
+                    _context.Servers.Add(newlyCreated);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("");
+                }
+                else
+                {
+                    ViewData["Exception"] = "An error occured!";
+                    return View();
+                }
+            }
+            catch (AmazonEC2Exception e)
+            {
+                ViewData["Exception"] = e.Message;
+                return View();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
