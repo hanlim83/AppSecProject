4c1368dde2774cb89c4b96c9b5e8307fd5cb9876
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-06 22:12:28 +0800

    Better support to view Challenges in UserSide
    To remove unused pages in future updates

---
+++ UserSide/Controllers/ChallengesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UserSide.Data;
+using UserSide.Models;
+
+namespace UserSide.Controllers
+{
+    public class ChallengesController : Controller
+    {
+        private readonly CompetitionContext _context;
+
+        public ChallengesController(CompetitionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Challenges
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var competition = await _context.Competitions
+                .Include(c => c.CompetitionCategories)
+                .Include(c1 => c1.Challenges)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (competition == null)
+            {
+                return NotFound();
+            }
+
+            return View(competition);
+        }
+
+        // GET: Challenges/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var challenge = await _context.Challenge
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(challenge);
+        }
+
+        // GET: Challenges/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Challenges/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Description,Value,Flag,CompetitionID,CompetitionCategoryID")] Challenge challenge)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(challenge);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(challenge);
+        }
+
+        // GET: Challenges/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var challenge = await _context.Challenge.FindAsync(id);
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+            return View(challenge);
+        }
+
+        // POST: Challenges/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Description,Value,Flag,CompetitionID,CompetitionCategoryID")] Challenge challenge)
+        {
+            if (id != challenge.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(challenge);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ChallengeExists(challenge.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(challenge);
+        }
+
+        // GET: Challenges/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var challenge = await _context.Challenge
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (challenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(challenge);
+        }
+
+        // POST: Challenges/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var challenge = await _context.Challenge.FindAsync(id);
+            _context.Challenge.Remove(challenge);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ChallengeExists(int id)
+        {
+            return _context.Challenge.Any(e => e.ID == id);
+        }
+    }
+}
