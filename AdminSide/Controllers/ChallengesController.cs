a0b703cc4bb49e7f419d527493cc2ebea6608fdc
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-10 01:30:34 +0800

    Added basic support for file storage
    To test for bugs in the future and style UI

--- AdminSide/Controllers/ChallengesController.cs
+++ AdminSide/Controllers/ChallengesController.cs
@@ -6,16 +6,24 @@ using AdminSide.Data;
 using AdminSide.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using Amazon.S3.Transfer;
+using Amazon.S3;
 
 namespace AdminSide.Controllers
 {
     public class ChallengesController : Controller
     {
+        IAmazonS3 S3Client { get; set; }
+
         private readonly CompetitionContext _context;
 
-        public ChallengesController(CompetitionContext context)
+        public ChallengesController(CompetitionContext context, IAmazonS3 s3Client)
         {
             _context = context;
+            this.S3Client = s3Client;
         }
 
         // GET: Challenges
@@ -103,19 +111,57 @@ namespace AdminSide.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,Description,Value,Flag,CompetitionID,CompetitionCategoryID")] Challenge challenge)
+        public async Task<IActionResult> Create([Bind("ID,Name,Description,Value,Flag,CompetitionID,CompetitionCategoryID")] Challenge challenge, List<IFormFile> files)
         {
+            var competition = await _context.Competitions
+                .Include(c => c.CompetitionCategories)
+                .Include(c1 => c1.Challenges)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == challenge.CompetitionID);
+
+            foreach (var category in competition.CompetitionCategories)
+            {
+                if (category.ID == challenge.CompetitionCategoryID)
+                {
+                    foreach (var file in files)
+                    {
+                        await UploadFileToS3(file, competition.BucketName, category.CategoryName);
+                    }
+                }
+            }
+            //Testing codes
+            
+            //Testing codes
+
             if (ModelState.IsValid)
             {
-                //hardcoded data for all information set to first CTF first Category
-                //challenge.CompetitionID = 1;
-                //challenge.CompetitionCategoryID = 1;
+                //call method to add file
                 _context.Add(challenge);
                 await _context.SaveChangesAsync();
                 //return RedirectToAction(nameof(Index));
                 return RedirectToAction("Index", "Challenges", new { id = challenge.CompetitionID });
             }
             return View(challenge);
+        }
+
+        //Upload file to S3
+        public async Task UploadFileToS3(IFormFile file, string bucketName, string folderName)
+        {
+            using (var newMemoryStream = new MemoryStream())
+            {
+                file.CopyTo(newMemoryStream);
+
+                var uploadRequest = new TransferUtilityUploadRequest
+                {
+                    InputStream = newMemoryStream,
+                    Key = folderName + "/" + file.FileName,
+                    BucketName = bucketName,
+                    CannedACL = S3CannedACL.PublicRead
+                };
+
+                var fileTransferUtility = new TransferUtility(S3Client);
+                await fileTransferUtility.UploadAsync(uploadRequest);
+            }
         }
 
         // GET: Challenges/Edit/5
