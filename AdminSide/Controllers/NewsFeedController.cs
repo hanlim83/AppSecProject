1b7b79288da593abb571f2073d7e7b1e2c186c42
Karen <hyunjaekaren@gmail.com>
2019-01-10 00:18:05 +0800

    Reading RSS Feed
    source: https://www.c-sharpcorner.com/article/read-rss-feed-using-mvc/
    
    e.g of RSS:https://www.c-sharpcorner.com/members/sourabh-somani/articles

--- AdminSide/Controllers/NewsFeedController.cs
+++ AdminSide/Controllers/NewsFeedController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using AdminSide.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Web;
+using System.Xml.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +19,26 @@ namespace AdminSide.Controllers
 
         public IActionResult SearchPage()
         {
+            return View();
+        }
+
+        // RSS FEED Codes
+        [HttpPost]
+        public ActionResult Index (string RSSURL)
+        {
+            WebClient wclient = new WebClient();
+            string RSSDate = wclient.DownloadString(RSSURL);
+
+            XDocument xml = XDocument.Parse(RSSDate);
+            var RSSFeedData = (from x in xml.Descendants("item") select new RSSFeed
+            {
+                Title = ((string)x.Element("title")),
+                Link = ((string)x.Element("link")),
+                Description = ((string)x.Element("description")),
+                PubDate = ((string)x.Element("pubDate"))
+            });
+            ViewBag.RSSFeed = RSSFeedData;
+            ViewBag.URL = RSSURL;
             return View();
         }
     }
---
+++ AdminSide/Models/RSSFeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AdminSide.Models
+{
+    public class RSSFeed
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public String Description { get; set; }
+        public string PubDate { get; set; }
+  
+    }
+}
