5ab5678dd9b4b8707d9d1c3269c8378549695d2e
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-10 23:26:57 +0800

    Update EmailSender to make email source more legitimate

--- UserSide/Services/EmailSender.cs
+++ UserSide/Services/EmailSender.cs
@@ -18,9 +18,7 @@ namespace UserSide.Areas.Identity.Services
 
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            //var apiKey = "SG.FrVJESKYRDibvQzII3Fng.o4UA5tsXZg3nCSirwKmEoYgHr2iZeurG_GD0gChBiq0";
             return Execute(Options.SendGridKey, subject, message, email);
-            //await testFunctionAsync();
             //return Execute(apiKey, subject, message, email);
         }
 
@@ -30,7 +28,7 @@ namespace UserSide.Areas.Identity.Services
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
-                From = new EmailAddress("Joe@contoso.com", "Joe Smith"),
+                From = new EmailAddress("noreply@ectf.com", "eCTF Email Confirmation"),
                 Subject = subject,
                 PlainTextContent = message,
                 HtmlContent = message
@@ -42,20 +40,6 @@ namespace UserSide.Areas.Identity.Services
             msg.SetClickTracking(false, false);
 
             return client.SendEmailAsync(msg);
-        }
-
-        public async Task testFunctionAsync()
-        {
-            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-            //var apiKey = "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";
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@example.com", "Example User");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("hugochiaxyz@gmail.com", "Example User");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
         }
     }
 }
