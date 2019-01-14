77f8b978b5f5784a17d41757e91606d65090eb30
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-14 23:53:07 +0800

    Added Support for Font Awesome Icons
    To remove google icons support in the future

--- UserSide/Controllers/CompetitionsController.cs
+++ UserSide/Controllers/CompetitionsController.cs
@@ -10,6 +10,8 @@ using UserSide.Data;
 using UserSide.Models;
 
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace UserSide.Controllers
 {
@@ -18,10 +20,12 @@ namespace UserSide.Controllers
     public class CompetitionsController : Controller
     {
         private readonly CompetitionContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public CompetitionsController(CompetitionContext context)
+        public CompetitionsController(CompetitionContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Competitions
@@ -66,6 +70,8 @@ namespace UserSide.Controllers
             return View(competition);
         }
 
+        
+
         public async Task<IActionResult> SignUp(int? id)
         {
             if (id == null)
@@ -87,6 +93,24 @@ namespace UserSide.Controllers
             TeamCreateViewModel teamCreateViewModel = new TeamCreateViewModel();
             teamCreateViewModel.Competition = competition;
 
+            //Need to get user.Id
+            // var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userName = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            //var user = await _userManager.FindByNameAsync(userName);
+            foreach (var Team in competition.Teams)
+            {
+                foreach (var TeamUser in Team.TeamUsers)
+                {
+                    if (TeamUser.UserId.Equals(userName))
+                    {
+                        return RedirectToAction("Index", "Competitions");
+                    }
+                    else
+                    {
+                        return View(teamCreateViewModel);
+                    }
+                }
+            }
             return View(teamCreateViewModel);
             //return View();
         }
