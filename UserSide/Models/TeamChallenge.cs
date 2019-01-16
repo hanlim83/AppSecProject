b0341d92172f7403c2bbb7165d25968417afcd06
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-17 02:46:48 +0800

    Add model for object binding for security checks

---
+++ UserSide/Models/TeamChallenge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSide.Models
+{
+    public class TeamChallenge
+    {
+        [Key]
+        public int TeamUserID { get; set; }
+
+        [ForeignKey("TeamId")]
+        public int TeamId { get; set; }
+        [ForeignKey("ChallengeId")]
+        public int ChallengeId { get; set; }
+    }
+}
