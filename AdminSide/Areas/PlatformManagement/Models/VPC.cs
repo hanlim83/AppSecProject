a838e4198efad53b08fe79c93ce7d734ea0e938b
Wheatley <30753183+hanlim83@users.noreply.github.com>
2019-01-21 23:32:29 +0800

    Added Setup script to import existing subnets and route tables

--- AdminSide/Areas/PlatformManagement/Models/VPC.cs
+++ AdminSide/Areas/PlatformManagement/Models/VPC.cs
@@ -2,10 +2,15 @@
 
 namespace AdminSide.Areas.PlatformManagement.Models
 {
+    public enum VPCType
+    {
+        Platform,DefaultVM,VM
+    }
     public class VPC
     {
         public int ID { get; set; }
         public string AWSVPCReference { get; set; }
         public string AWSVPCDefaultSecurityGroup { get; set; }
+        public VPCType type { get; set; }
     }
 }
