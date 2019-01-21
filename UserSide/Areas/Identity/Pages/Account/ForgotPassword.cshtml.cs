d78a856fd47c7bc0d41a0618eb84185dfe49468c
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-21 14:19:56 +0800

    Refactored pages to Container class
    Also removed 1 unused import

--- UserSide/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ UserSide/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 
 namespace UserSide.Areas.Identity.Pages.Account
