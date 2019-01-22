977b53b9d324aeb4ea7cce7316b2b85255d70114
evelyngotbite <elxwy@users.noreply.github.com>
2019-01-22 14:08:48 +0800

    Updating Errors
    At Admin Side

--- AdminSide/Models/PostViewModel.cs
+++ AdminSide/Models/PostViewModel.cs
@@ -8,7 +8,7 @@ namespace AdminSide.Models
     public class PostViewModel
     {
         public IEnumerable<ForumCategory> ForumCategory { get; set; }
-        public Post Post { get; set; }
+        public Post post { get; set; }
         public List<Comment> Comments { get; set; }
     }
 }
