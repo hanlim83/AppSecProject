3dfa386140971fcc25e96731176a4ae9edd259ba
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-26 16:37:21 +0800

    Added base layout for viewing/adding admin

---
+++ AdminSide/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminSide.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var adminList = await _userManager.Users.ToListAsync();
+
+            ViewBag.adminList = adminList;
+
+            return View();
+        }
+    }
+}
