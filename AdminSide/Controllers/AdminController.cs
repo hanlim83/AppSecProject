aeeb28eb52286215573ad3702f6c17134b340fb4
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-26 17:05:27 +0800

    Improved support for adding new admin
    UI updates included

--- AdminSide/Controllers/AdminController.cs
+++ AdminSide/Controllers/AdminController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace AdminSide.Controllers
 {
+    [Authorize]
+    //the line above makes a page protected and will redirect user back to login
     public class AdminController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
