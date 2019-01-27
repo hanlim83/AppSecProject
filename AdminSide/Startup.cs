13de709163349c2c1b2b98de6b03a9a95b9e37c5
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2019-01-27 16:12:41 +0800

    Added email verification to admin accounts

--- AdminSide/Startup.cs
+++ AdminSide/Startup.cs
@@ -1,4 +1,5 @@
-﻿using AdminSide.Areas.PlatformManagement.Data;
+﻿using AdminSide.Areas.Identity.Services;
+using AdminSide.Areas.PlatformManagement.Data;
 using AdminSide.Areas.PlatformManagement.Services;
 using AdminSide.Data;
 using Amazon.CloudWatch;
@@ -112,9 +113,9 @@ namespace AdminSide
             options.UseSqlServer(
             GetRdsConnectionStringIdentity()));
 
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            //services.AddIdentity<IdentityUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>()
+            //    .AddDefaultTokenProviders();
 
             //Competition Db Context
             services.AddDbContext<CompetitionContext>(options =>
@@ -163,8 +164,12 @@ namespace AdminSide
                 options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
             });
 
-            // using Microsoft.AspNetCore.Identity.UI.Services;
+            //// using Microsoft.AspNetCore.Identity.UI.Services;
+            //services.AddSingleton<IEmailSender, EmailSender>();
+
+            // requires
             services.AddSingleton<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
 
             //Core AWS Initialization
             var awsOptions = Configuration.GetAWSOptions();
@@ -234,11 +239,11 @@ namespace AdminSide
         }
     }
 
-    public class EmailSender : IEmailSender
-    {
-        public Task SendEmailAsync(string email, string subject, string message)
-        {
-            return Task.CompletedTask;
-        }
-    }
+    //public class EmailSender : IEmailSender
+    //{
+    //    public Task SendEmailAsync(string email, string subject, string message)
+    //    {
+    //        return Task.CompletedTask;
+    //    }
+    //}
 }
